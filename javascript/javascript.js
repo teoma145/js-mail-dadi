adc460ac7594fd2138c610d486411150afb862cb
teoma145 <matteodark145@yahoo.it>
2023-10-12 16:03:10 +0200

    variabili

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+const buttonel = document.getElementById('button');
+
+const myarray =[
+    'mariorossi@gmail.com,',
+    'mariorossi@gmail.com',
+    'marcoromani@gmail.com',
+    'mirkosabati@gmail.com',
+]
+ 
+buttonel.addEventListener('click',function(){
+ let mailel = document.getElementById('mailel').value;
+ console.log(mailel)
+})
+
+
+
+
